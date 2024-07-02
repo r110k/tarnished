f116fa527c4ba68abca53c2fb8d8cce1b49f5c2b
ArcGua <archergua@hotmail.com>
2024-07-02 12:53:50 +0800

    使用 useRef 解决内存泄漏的问题

--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -1,27 +1,24 @@
 import { animated, useTransition } from '@react-spring/web'
-import { type ReactNode } from 'react'
-import { Outlet, useLocation, useOutlet } from 'react-router-dom'
+import type { ReactNode } from 'react'
+import { useRef } from 'react'
+import { useLocation, useOutlet } from 'react-router-dom'
 
-const map: Record<string, ReactNode> = {}
 export const WelcomeLayout: React.FC = () => {
+  const map = useRef<Record<string, ReactNode>>({})
   const location = useLocation()
   const outlet = useOutlet()
   const transitions = useTransition(location.pathname, {
     from: {
-      transform: location.pathname === '/wecome/1'
-        ? 'translateX(0)'
-        : 'translateX(100%)',
+      transform: location.pathname === '/welcome/1' ? 'translateX(0)' : 'translateX(100%)',
     },
     enter: { transform: 'translateX(0%)' },
     leave: { transform: 'translateX(-100%)' },
-    config: { duration: 600 },
+    config: { duration: 500 },
   })
-  map[location.pathname] = outlet
-  return transitions((style, pathname) => {
-    return <animated.div key={pathname} style={style}>
-      <div style={{ textAlign: 'center' }}>
-        {map[pathname]}
-      </div>
-    </animated.div>
-  })
+  map.current[location.pathname] = outlet
+  return transitions((style, pathname) =>
+     <animated.div key={pathname} style={style}>
+      {map.current[pathname]}
+    </animated.div>,
+  )
 }
