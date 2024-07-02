a5b1830729e058459d2c1ca60c40b3e45252cb81
xiaogua07 <xiaogua07@outlook.com>
2024-07-02 22:59:46 +0800

    成功配置 unocss

--- src/pages/Welcome4.tsx
+++ src/pages/Welcome4.tsx
@@ -1,10 +1,13 @@
-import { NavLink } from 'react-router-dom'
+import * as React from 'react'
 
 export const Welcome4: React.FC = () => {
-  return (
-    <div style={{ border: '1px solid red' }}>
-        4
-        <NavLink to="/">开始记账</NavLink>
-    </div>
-  )
+  return (<div flex justify-center items-center
+      after="content-[hi] b-1 b-red"
+      before="content-[hi] b-1 b-red"
+      shadow
+    >
+      <header hover:bg-red w-100px b-1 b-red h-40px></header>
+      <main grow-1 b-1 b-blue h-100px></main>
+      <footer w-200px b-1 b-black h-55px></footer>
+    </div>)
 }
---
+++ src/shims.d.ts
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+declare module 'react' {
+  interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
+    flex?: boolean
+    relative?: boolean
+    grid?: boolean
+    shadow?: boolean
+    text?: string 
+    before?: string 
+    after?: string 
+  }
+}
