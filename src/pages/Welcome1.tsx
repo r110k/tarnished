d6a3c3484a45fb324c02770c512efcfccc84f8a8
xiaogua07 <xiaogua07@outlook.com>
2024-07-02 22:43:15 +0800

    Use module css

--- src/pages/Welcome1.tsx
+++ src/pages/Welcome1.tsx
@@ -1,8 +1,9 @@
 import { NavLink } from 'react-router-dom'
+import s from './Welcome1.module.scss'
 
 export const Welcome1: React.FC = () => {
   return (
-    <div style={{ border: '1px solid red' }}>
+    <div className={s.wrapper} >
         1
         <NavLink to="/welcome/2">下一页</NavLink>
     </div>
