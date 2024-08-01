a2f2f97f92a7da5e98799ec72a062f1831d923da
Springatom <Springatom@hotmail.com>
2024-08-01 22:16:32 +0800

    安装 swr & axios

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,19 @@
+import useSWR from 'swr'
+import axios from 'axios'
+
+const fetcher = (path: string) => {
+  return axios.get<{ msg: string }>(path)
+}
+
 export const Home: React.FC = () => {
-  return (
-    <div text-6xl>
-      Home Display zustand
-    </div>
-  )
+  const { data, error } = useSWR('http://152.32.233.140:3000', fetcher)
+  console.log('data?.data')
+  console.log(data?.data)
+  if (error) {
+    return <div>Failed to load</div>
+  }
+  if (!data) {
+    return <div>Loading</div>
+  }
+  return <div>hello, {data?.data.msg}</div>
 }
