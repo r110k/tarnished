7287d67bc5d8f09c512f95c083d38402040844bd
r110k <r110k@foxmail.com>
2024-08-10 00:19:28 +0800

    完成 TopNav 组件

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -1,5 +1,14 @@
-export const TopNav: React.FC = () => {
+import { Icon } from './Icon'
+
+interface Props {
+  title?: string
+}
+
+export const TopNav: React.FC<Props> = ({ title = '褪色者啊' }) => {
   return (
-    <div>TopNav </div>
+    <div text-white flex items-center p-16px>
+      <Icon name="menu" className='w-24px h-24px mr-16px' />
+      <h1 text-24px leading-none>{title}</h1>
+    </div>
   )
 }
