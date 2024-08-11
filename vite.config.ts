b3c2d1da659bb8dc90f951223c9cc26ec2289646
r110k <r110k@foxmail.com>
2024-08-11 22:50:47 +0800

    优化引入 svg 插件，可以指定部分 svg 不去色

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,6 @@ export default defineConfig(({ command }) => ({
       mockPath: 'mock',
       localEnabled: command === 'serve',
     }),
-    svgsprites(),
+    svgsprites({ noOptimizeList: ['chart', 'category', 'export', 'noty'] }),
   ],
 }))
