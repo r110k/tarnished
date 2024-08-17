96bda77d1ef891ec7eb38fa30b7aa3ac2921a5c0
r110k <r110k@foxmail.com>
2024-08-18 00:57:22 +0800

    使用 visibility: hidden|visible 解决遮罩层不能点击问题,引入maskvisible关联mask的visibility属性，使用动画的onStart&onRest钩子解决退出动画直接消失的问题

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,4 +1,5 @@
 import { animated, useSpring } from '@react-spring/web'
+import { useState } from 'react'
 
 type Props = {
   visible: boolean
@@ -8,10 +9,28 @@ type Props = {
 export const Popup: React.FC<Props> = (props) => {
   const { visible, onClickMask } = props
 
+  const [maskVisible, setMaskVisible] = useState(visible)
+
   const maskStyles = useSpring({
     opacity: visible ? 0.75 : 0,
+    onStart: ({ value }) => {
+      if (value.opacity < 0.5) {
+        setMaskVisible(true)
+      }
+    },
+    onRest: ({ value }) => {
+      if (value.opacity < 0.05) {
+        setMaskVisible(false)
+      }
+    },
+    config: {
+      duration: 300,
+    },
   })
-
+  const maskStyles2 = {
+    ...maskStyles,
+    visibility: (maskVisible ? 'visible' : 'hidden') as 'visible' | 'hidden',
+  }
   const menuStyles = useSpring({
     opacity: visible ? 1 : 0,
     transform: visible ? 'translateY(0%)' : 'translateY(100%)',
@@ -23,7 +42,7 @@ export const Popup: React.FC<Props> = (props) => {
   return (
     <div>
       <animated.div fixed top-0 left-0 h-full w-full bg-black opacity-75
-        onClick={() => onClickMask?.()} style={maskStyles}
+        onClick={() => onClickMask?.()} style={maskStyles2}
         z="[calc(var(--z-popup)-1)]">
       </animated.div>
       <animated.div fixed bottom-0 left-0 w-full min-h-100px bg-white
