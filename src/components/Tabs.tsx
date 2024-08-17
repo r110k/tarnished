68378c6a7697442428c08d22a07307357770f781
r110k <r110k@foxmail.com>
2024-08-17 10:56:07 +0800

    完成记账页面的顶部

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -3,11 +3,12 @@ interface Props<T> {
   tabItems: { key: T; text: string }[]
   value: T
   onChange: (key: T) => void
+  className?: string
 }
 export const Tabs = <T extends string>(props: Props<T>) => {
-  const { tabItems, value, onChange } = props
+  const { tabItems, value, onChange, className } = props
   return (
-    <ol flex text-white children-px-24px children-py-12px>
+    <ol className={className} flex text-white children-px-24px children-py-12px>
       {tabItems.map(items => (<li key={items.key}
         className={items.key === value ? s.selected : ''}
         onClick={() => onChange(items.key)}>
--- src/pages/ItemsNewPage.tsx
+++ src/pages/ItemsNewPage.tsx
@@ -1,12 +1,22 @@
+import { useState } from 'react'
 import { Gradient } from '../components/Gradient'
 import { Icon } from '../components/Icon'
+import { Tabs } from '../components/Tabs'
 import { TopNav } from '../components/TopNav'
 
+export type ItemKind = 'income' | 'expenses'
+const tabItems: { key: ItemKind; text: string }[] = [
+  { key: 'expenses', text: '支出' },
+  { key: 'income', text: '收入' },
+]
 export const ItemsNewPage: React.FC = () => {
+  const [tabItem, setTabItem] = useState<ItemKind>('expenses')
   return (
     <div>
       <Gradient>
         <TopNav title="使用你的卢恩" icon={ <Icon name="back" /> } />
+        <Tabs tabItems={tabItems} value={tabItem} onChange={setTabItem} 
+          className='children-flex-1 text-center'/>
       </Gradient>
     </div>
   )
