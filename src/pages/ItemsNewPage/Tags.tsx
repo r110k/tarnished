287b23f2c6a4784d2513598d98efbdee95e692f6
r110k <r110k@foxmail.com>
2024-08-17 12:33:27 +0800

    完善 tags 组件的样式

--- src/pages/ItemsNewPage/Tags.tsx
+++ src/pages/ItemsNewPage/Tags.tsx
@@ -1,3 +1,5 @@
+import { Icon } from '../../components/Icon'
+
 interface Props {
   kind: Item['kind']
 }
@@ -7,9 +9,20 @@ export const Tags: React.FC<Props> = () => {
     <div>
       <ol grid grid-cols="[repeat(auto-fit,48px)]"
         justify-center gap-x-32px gap-y-16px px-32px py-16px>
+        <li w-48px flex justify-center items-center flex-col gap-y-8px>
+          <span block w-48px h-48px rounded="24px" bg="[rgb(173,170,120)]"
+            text-24px flex justify-center items-center>
+              <Icon name="add" className="text-[rgb(59,65,48)]" />
+          </span>
+          <span text-12px>新增标签</span>
+        </li>
         {tags.map(tag => (
-          <li w-48px h-48px flex justify-center items-center
-            b-1 b="[rgb(173,170,120)]">🚀</li>
+          <li w-48px flex justify-center items-center flex-col gap-y-8px>
+            <span block w-48px h-48px rounded="24px" bg="[rgb(173,170,120)]"
+              text-24px flex justify-center items-center
+              b-1 b="[rgb(59,65,48)]">🚀</span>
+            <span text-12px text="#666">打车</span>
+          </li>
         ))}
       </ol>
     </div>
