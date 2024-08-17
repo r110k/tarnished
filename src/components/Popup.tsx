aed5d399a314259e9140732dcfdd772d6fdb4729
r110k <r110k@foxmail.com>
2024-08-18 00:49:36 +0800

    内敛 visible 逻辑，添加初版动画，遇到隐藏遮罩层挡住按钮的问题

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -7,21 +7,29 @@ type Props = {
 //  bg-black opacity-75 等价于 className="bg-black:75"
 export const Popup: React.FC<Props> = (props) => {
   const { visible, onClickMask } = props
+
   const maskStyles = useSpring({
     opacity: visible ? 0.75 : 0,
   })
 
+  const menuStyles = useSpring({
+    opacity: visible ? 1 : 0,
+    transform: visible ? 'translateY(0%)' : 'translateY(100%)',
+    config: {
+      duration: 300,
+    },
+  })
+
   return (
-    visible
-      ? <div>
-          <animated.div fixed top-0 left-0 h-full w-full bg-black opacity-75
-            onClick={() => onClickMask?.()} style={{ ...maskStyles }}
-            z="[calc(var(--z-popup)-1)]">
-          </animated.div>
-          <animated.div fixed bottom-0 left-0 w-full min-h-100px bg-white
-            z="[var(--z-popup)]">
-          </animated.div>
-        </div>
-      : null
+    <div>
+      <animated.div fixed top-0 left-0 h-full w-full bg-black opacity-75
+        onClick={() => onClickMask?.()} style={maskStyles}
+        z="[calc(var(--z-popup)-1)]">
+      </animated.div>
+      <animated.div fixed bottom-0 left-0 w-full min-h-100px bg-white
+        style={menuStyles}
+        z="[var(--z-popup)]">
+      </animated.div>
+    </div>
   )
 }
