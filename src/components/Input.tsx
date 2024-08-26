f7a6fb4e806a6fc079f75a950a20eea413028c09
r110k <r110k@foxmail.com>
2024-08-26 16:40:05 +0800

    可以使用中文空格解决验证码错误信息提示高度切换的问题

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,7 +3,7 @@ import { EmojiInput } from './Input/EmojiInput'
 type Props = {
   label: string
   placeholder?: string
-  type?: 'text' | 'emoji'
+  type?: 'text' | 'emoji' | 'sms_code'
   value?: string
   onChange?: (value: string) => void
   error?: string
@@ -18,7 +18,15 @@ export const Input: React.FC<Props> = (props) => {
         return <EmojiInput />
       case 'text':
         return <input g-input-text type={type} placeholder={placeholder || '请输入'}
-        value={value} onChange={e => onChange?.(e.target.value)} />
+          value={value} onChange={e => onChange?.(e.target.value)} />
+      case 'sms_code':
+        return (
+          <div flex gap-x-16px>
+            <input max-w="[calc(40%-8px)]" g-input-text type="text" placeholder={placeholder}
+              value={value} onChange={e => onChange?.(e.target.value)} />
+            <button shrink-0 max-w="[calc(60%-8px)]" g-btn>发送验证码</button>
+          </div>
+        )
       default:
         return null
     }
@@ -27,8 +35,8 @@ export const Input: React.FC<Props> = (props) => {
   return (
     <div flex flex-col gap-y-8px>
       <span text-18px >{label}</span>
-      { renderInput() }
-      <span text-14px text-red>{error || '\u00A0'}</span>
+      {renderInput()}
+      <span text-14px text-red>{error || '\u3000'}</span>
     </div>
   )
 }
