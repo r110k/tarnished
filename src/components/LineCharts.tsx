dd4438ab5688fb42f7eed4686aaed752352f668d
r110k <r110k@foxmail.com>
2024-08-28 08:58:56 +0800

    基本完善折线图的样式

--- src/components/LineCharts.tsx
+++ src/components/LineCharts.tsx
@@ -16,13 +16,36 @@ export const LineCharts: React.FC<Props> = (props) => {
   useEffect(() => {
     if (!divRef.current) { return }
     const myChart = echarts.init(divRef.current)
-    const option = {
+    const option: echarts.EChartsOption = {
+      tooltip: {
+        show: true,
+        trigger: 'axis',
+        formatter: ([{ axisValue, data }]: any) => {
+          const parts = axisValue.split('-')
+          const label = `${parts[0]}年${parts[1]}月${parts[2]}日`
+          const value = data === null ? '无数据' : `${data}元`
+          return `${label}<br /><div text-right>${value}</div>`
+        },
+      },
+      grid: {
+        top: 8,
+        right: 16,
+        bottom: 24,
+        left: 16,
+      },
       xAxis: {
         type: 'category',
         data: xItems,
+        axisLabel: {
+          // label from '2021-12-01' to '01-01'
+          formatter: (label: string) => label.slice(label.indexOf('-') + 1),
+        },
       },
       yAxis: {
         type: 'value',
+        axisLabel: {
+          show: false,
+        },
       },
       series: [
         {
