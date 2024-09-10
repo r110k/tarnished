a576ff89ff6ffd89a0ba308e62fc3e81f980a296
r110k <r110k@foxmail.com>
2024-09-10 19:30:38 +0800

    本地支持 ssl https 链接

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
+import type { ConfigEnv } from 'vite'
 import { defineConfig } from 'vite'
 import { viteMockServe } from 'vite-plugin-mock'
 import react from '@vitejs/plugin-react'
 import UnoCSS from 'unocss/vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 import { svgsprites } from './vite_plugins/svgsprites'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig(({ command }: ConfigEnv) => ({
   define: {
     isDev: command === 'serve',
   },
   plugins: [
-    UnoCSS(),
+    basicSsl(),
+    UnoCSS() as any,
     react(),
     viteMockServe({
       mockPath: 'mock',
